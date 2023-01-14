4ecdfa15ff8c6cd03b1f15bd33d0853e749d740e
rinagatomy <rinagatomy@paschoalotto.com.br>
2023-01-14 12:20:02 -0300

    feat(UserDelete): Add card for delete user

--- src/app/screens/user/user-delete/user-delete.component.ts
+++ src/app/screens/user/user-delete/user-delete.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'lab-user-delete',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './user-delete.component.html',
-  styleUrls: ['./user-delete.component.scss']
+  styleUrls: ['./user-delete.component.scss'],
 })
 export class UserDeleteComponent {
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
+  onCancel() {
+    this.router.navigate(['../../'], { relativeTo: this.route });
+  }
 }
