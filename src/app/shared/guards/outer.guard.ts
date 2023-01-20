8e64e90b7e8a233bf2869dad0a18242fe1c05fa4
rinagatomy <rinagatomy@paschoalotto.com.br>
2023-01-20 19:40:04 -0300

    refactor(OuterGuards): Refactor imports

--- src/app/shared/guards/outer.guard.ts
+++ src/app/shared/guards/outer.guard.ts
@@ -1,14 +1,6 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  CanLoad,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
-import { Observable } from 'rxjs';
-import { DASHBOARD, LOGIN } from 'src/app/application/routes';
+import { CanActivate, Router } from '@angular/router';
+import { DASHBOARD } from 'src/app/application/routes';
 import { LocalService } from '../services/storage/local.service';
 
 @Injectable({
