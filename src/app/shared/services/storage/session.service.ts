9b31692ee8c7af464ccf73a7d9edc561ff57abed
rinagatomy <rinagatomy@paschoalotto.com.br>
2023-01-21 10:51:36 -0300

    feat(sessionStorage): Add session storage

--- src/app/shared/services/storage/session.service.ts
+++ src/app/shared/services/storage/session.service.ts
@@ -1,6 +1,26 @@
 import { Injectable } from '@angular/core';
+import { BaseStorageService } from './base-storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class SessionService {}
+export class SessionService extends BaseStorageService {
+  protected session: Storage;
+
+  constructor() {
+    super();
+    this.session = sessionStorage;
+  }
+  getToken(): string | null {
+    return this.session.getItem(this.key);
+  }
+  setToken(token: string): void {
+    this.session.setItem(this.key, token);
+  }
+  removeToken(): void {
+    this.session.removeItem(this.key);
+  }
+  clearTokens(): void {
+    this.session.clear();
+  }
+}
